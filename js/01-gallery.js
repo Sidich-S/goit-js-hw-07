1eab4c6002bdd1cf7bdc2eab8366e983334e8944
Sidich-S <106704084+Sidich-S@users.noreply.github.com>
2022-10-12 21:03:22 +0300

    change

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,30 +1,45 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
 
-const galleryRef = document.querySelector('.gallery')
-const makeGallery = galleryItems => {
+const galleryRef = document.querySelector(".gallery")
 
-    return galleryItems.map(item => {
-        const galleryItem = document.createElement('div');
-        galleryItem.classList.add('gallery__item');
-
-        const galleryLink = document.createElement('a');
-        galleryLink.href = (item.original);
-        galleryItem.appendChild(galleryLink);
-
-        const galleryImg = document.createElement('img');
-        galleryImg.classList.add('gallery__image');
-        galleryImg.src = (item.preview);
-        galleryImg.alt = (item.description);
-        galleryImg.getAttribute = ('data-source', item.original);
-        galleryLink.appendChild(galleryImg);
-
-        galleryRef.append(galleryItem);
-        return galleryItem
-    });
+function makeGallery(gallery) {
+    return gallery.map(({ preview, original, description }) => `
+    <div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img loading="lazy"
+             class="gallery__image"
+             src="${preview}"
+             data-source="${original}"
+             alt="${description}"/>
+      </a>
+    </div>`).join("")
 }
+  
+const onGallery = makeGallery(galleryItems)
+galleryRef.insertAdjacentHTML("beforeend", onGallery)
 
-console.log(basicLightbox);
-makeGallery(galleryItems);
+// ---------------------------------------------------------------
+galleryRef.addEventListener("click", makeDigImg)
+
+function makeDigImg(event) {
+  event.preventDefault()
+  if (event.target.nodeName !== "IMG") {
+    return
+  }
+    const choice = basicLightbox.create(`<img src = "${event.target.dataset.source}" 
+        width="800" height="600"/>`)
+        choice.show();
+  
+    document.addEventListener("keydown", function closePicture (event){
+    console.log(event.key, event.code);
+    if (event.code !== "Escape") {
+      return
+    }
+    choice.close()
+    document.removeEventListener("keydown", closePicture)
+  });
+  
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,48 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+// const galleryRef = document.querySelector('.gallery')
+// const makeGallery = galleryItems => {
+
+//     return galleryItems.map(item => {
+//         const galleryItem = document.createElement('div');
+//         galleryItem.classList.add('gallery__item');
+
+//         const galleryLink = document.createElement('a');
+//         galleryLink.href = (item.original);
+//         galleryItem.appendChild(galleryLink);
+
+//         const galleryImg = document.createElement('img');
+//         galleryImg.classList.add('gallery__image');
+//         galleryImg.src = (item.preview);
+//         galleryImg.alt = (item.description);
+//         galleryImg.getAttribute = ('data-source', item.original);
+//         galleryLink.appendChild(galleryImg);
+
+//         galleryRef.append(galleryItem);
+//         return galleryItem
+//     });
+// }
+
+// console.log(basicLightbox);
+// makeGallery(galleryItems);
+
+const galleryRef = document.querySelector(".gallery");
+const lineGallery =  makeGallery(galleryItems);
+
+
+galleryRef.insertAdjacentHTML("afterbegin", lineGallery);
+
+function  makeGallery(galleryItems) {
+	return galleryItems
+		.map(({ preview, original, description }) => {
+			return `<li>
+                <a class="gallery__item" href="${original}">
+                  <img class="gallery__image" src="${preview}" alt="${description}"/>
+                </a>
+              </li>`;
+    }).join("");
+}
+ 
+    const lightbox = new simpleLightbox(".gallery a", { captionsData: "alt", captionDelay: 250, });
